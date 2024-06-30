ffd43304600073669b48bc0831f9f40370779c19
Theo Fortune <theofortunefunction@gmail.com>
2024-06-30 18:47:56 +0100

    defining variable

--- app/src/main/java/com/typhon/firstcreateproject/MainActivity.kt
+++ app/src/main/java/com/typhon/firstcreateproject/MainActivity.kt
@@ -11,6 +11,25 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContentView(R.layout.activity_main)
+		// Read Only: Cannot be changed again
+		val myExampleInt : Int
+		// Values that can be manipulated or mutable
+		var myExampleVarInt : Int
+
+		val lecturerName : String = "Fortune"
+		val foundedYearOfTechFusionCamp = 2024
+		val pi: Float = 3.147f
+		var currentYear : Number = 2024
+		var isRaining : Boolean = true
+		val kotlinIsBest : Boolean = true
+		val isJavaBetter : Boolean = false
+
+		// Var variables can be reassigned
+		currentYear = 2025
+		isRaining = true
+		println(currentYear)
+		println(isRaining)
+
 		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
